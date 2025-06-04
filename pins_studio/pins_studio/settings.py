bfabce6ddbdf83afa343f0880af14ca384951d53
prudhvi1519 <prudhviakula92@gmail.com>
2025-06-04 16:42:54 +0530

    Fix title length for Unsplash pins

--- pins_studio/pins_studio/settings.py
+++ pins_studio/pins_studio/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'pins' / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'pins' / 'static' / 'pins']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
