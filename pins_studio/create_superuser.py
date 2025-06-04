9de0cb32f225f2b6c6c19ccf6df33f7168304cbf
prudhvi1519 <prudhviakula92@gmail.com>
2025-06-04 11:57:25 +0530

    Fix syntax in create_superuser.py

--- pins_studio/create_superuser.py
+++ pins_studio/create_superuser.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import User
 from decouple import config
 def create_superuser():
-username = config('SUPERUSER_USERNAME', default='admin')
-email = config('SUPERUSER_EMAIL', default='admin@example.com')
-password = config('SUPERUSER_PASSWORD', default='admin123')
-if not User.objects.filter(username=username).exists():
-User.objects.create_superuser(username=username, email=email, password=password)
-print('Superuser created successfully.')
-else:
-print('Superuser already exists.')
-if name == 'main':
-create_superuser()
+    username = config('SUPERUSER_USERNAME', default='admin')
+    email = config('SUPERUSER_EMAIL', default='admin@example.com')
+    password = config('SUPERUSER_PASSWORD', default='admin123')
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print('Superuser created successfully.')
+    else:
+        print('Superuser already exists.')
+
+if __name__ == '__main__':
+    create_superuser()
