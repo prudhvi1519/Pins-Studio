30a6cee109234cf1c5d3472a5b869cc9b0cdf63c
prudhvi1519 <prudhviakula92@gmail.com>
2025-06-04 12:02:16 +0530

    Add logging for debugging

--- pins_studio/pins_studio/settings.py
+++ pins_studio/pins_studio/settings.py
@@ -100,3 +100,17 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') if ENVIRONMENT == 'local' else '/opt/render/project/src/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
