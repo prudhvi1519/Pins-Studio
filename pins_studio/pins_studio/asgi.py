f0dddc1b5436a979ee5d24f697524acb4e487384
prudhvi1519 <prudhviakula92@gmail.com>
2025-06-09 16:57:11 +0530

    Fix title length for Unsplash pins

--- pins_studio/pins_studio/asgi.py
+++ pins_studio/pins_studio/asgi.py
@@ -1,5 +1,16 @@
+"""
+ASGI config for pins_studio project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
 
 import os
+
 from django.core.asgi import get_asgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pins_studio.settings')
+
 application = get_asgi_application()
