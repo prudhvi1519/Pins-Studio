dcd778646e41f6cf9637cb808fecc93a9f1328f8
prudhvi1519 <prudhviakula92@gmail.com>
2025-06-10 15:05:49 +0530

    UI improved

--- pins_studio/staticfiles/pins/js/navbar_toggle.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    const toggler = document.querySelector('.navbar-toggler');
-    const collapse = document.querySelector('.navbar-collapse');
-
-    toggler.addEventListener('click', function () {
-        collapse.classList.toggle('show');
-    });
-})();
